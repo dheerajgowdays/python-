51312ceeabf5c9b8f69e66c1aa86cecf9441b467
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2025-03-17 21:38:17 +0530

    Update tip.py

--- tip.py
+++ tip.py
@@ -1,4 +1,12 @@
-cost=float(input("How much was the meal? $"))
-tip=int(input("What percentage would you like to tip? "))
-leave=cost*(tip/100)
-print("Leave: $ %2.f",leave)
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+def dollars_to_float(d):
+    d=float(d.replace("$"," "))
+    return d
+def percent_to_float(p):
+    p=float(p.replace("%"," "))
+    return p/100
+main()
