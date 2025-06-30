7a78276dc2ad460f0ed1d6018638977ac123cea7
dheerajgowdays <dheeraj76766@gmail.com>
2025-06-30 23:32:41 +0530

    updaing

--- einstein.py
+++ einstein.py
@@ -1,3 +1,4 @@
+#Even if you haven’t studied physics (recently or ever!), you might have heard that  wherein represents energy (measured in Joules), represents mass (measured in kilograms), and  represents the speed of light (measured approximately as 300000000 meters per second), per Albert Einstein et al. Essentially, the formula means that mass and energy are equivalent.In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms) and then outputs the equivalent number of Joules as an integer. Assume that the user will input an integer
 m=int(input("m:"))
 e=m*(3000000**2)
 print("E:",e)
