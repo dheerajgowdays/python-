534a90e98ab4f4b0f9bd56756aa9ee418577c35e
dheerajgowdays <dheeraj76766@gmail.com>
2025-07-05 23:52:54 +0530

    .

---
+++ taqueria.py
@@ -0,0 +1,25 @@
+#In a file called taqueria.py, implement a program that enables a user to place an order, prompting them for items, one per line, until the user inputs control-d (which is a common way of ending one’s input to a program). After each inputted item, display the total cost of all items inputted thus far, prefixed with a dollar sign ($) and formatted to two decimal places. Treat the user’s input case insensitively. Ignore any input that isn’t an item. Assume that every item on the menu will be titlecased.
+def main():
+    amount=0.0
+    menu={
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+    }
+    while True:
+        try:
+            order=input("Item: ").title()
+            if order in menu:
+                amount+=menu[order]
+                print(f"Total:${amount}")
+        except EOFError:
+            print(f"Toatal Amount:${amount}")
+            break
+                   
+main()
