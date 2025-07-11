5f7164cc9faa17d3f4bd2c144d32f3d1ba662b4f
dheerajgowdays <dheeraj76766@gmail.com>
2025-07-12 01:38:19 +0530

    .

--- figlet.py
+++ figlet.py
@@ -7,4 +7,28 @@ Prompts the user for a str of text.
 Outputs that text in the desired font.
 If the user provides two command-line arguments and the first is not -f or --font or the second is not the name of a font, the program should exit via sys.exit with an error message.
 """
-import 
+import sys
+import random
+from pyfiglet import Figlet
+
+def main():
+
+    figlet = Figlet()
+    fonts=figlet.getFonts()
+    args = sys.argv[1:]
+    if not args:
+        figlet.setFont(font=random.choice(fonts))
+    elif len(sys.argv) == 1:
+        choice_font=random.choice(sys.argv)
+        figlet.setFont(font=choice_font)
+    elif len(sys.argv) == 3 and sys.argv[1] in ['-f','-font']:
+        choice_font=sys.argv[2]
+        if choice_font not in fonts :
+            sys.exit("Invalid usage")
+        figlet.setFont(font=choice_font)
+    else:
+         sys.exit("Invalid usage")
+    sentance=input("Input:")
+    print(figlet.renderText(sentance))
+if __name__ == "__main__":
+    main()
