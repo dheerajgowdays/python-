42c4ba55eff3f754d75a2170645428dfa38a0ba0
dheerajgowdays <dheeraj76766@gmail.com>
2025-07-13 19:27:35 +0530

    .

---
+++ professor.py
@@ -0,0 +1,47 @@
+'''In a file called professor.py, implement a program that:
+
+Prompts the user for a level, 
+. If the user does not input 1, 2, or 3, the program should prompt again.
+Randomly generates ten (10) math problems formatted as X + Y = , wherein each of X and Y is a non-negative integer with 
+ digits. No need to support operations other than addition (+).
+ Prompts the user to solve each of those problems. If an answer is not correct (or not even a number), the program should output EEE and prompt the user again, allowing the user up to three tries in total for that problem. If the user has still not answered correctly after three tries, the program should output the correct answer.
+The program should ultimately output the user’s score: the number of correct answers out of 10.
+Structure your program as follows, wherein get_level prompts (and, if need be, re-prompts) the user for a level and returns 1, 2, or 3, and generate_integer returns a single randomly generated non-negative integer with level digits or raises a ValueError if level is not 1, 2, or 3:'''
+import random
+
+
+def main():
+    level=get_level()
+    generate_integer(level)
+    score
+
+def get_level():
+    while True:
+        try:
+            n= int(input("Level: "))
+            if 0<n>4:
+                return n
+                break
+            else:
+                continue
+        except ValueError:
+            continue
+def generate_integer(level):
+    i=1
+    while i<=10:
+        while True:
+            if level == 1:
+                x=random.randrange(1,9)
+                y=random.randrange(1,9)
+                z=x+y
+                try:
+                    sum=int(input(f"{x}+{y}="))
+                    if sum == z:
+                        continue
+                    else:
+                        raise ValueError("EEE")
+                except ValueError as e:
+                    print(e)
+                    continue
+if __name__ == "__main__":
+    main()
