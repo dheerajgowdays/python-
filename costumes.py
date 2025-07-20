76a7b5dc5d0997d1028f05ee7a308b1ea2ad8c9a
dheerajgowdays <dheeraj76766@gmail.com>
2025-07-20 22:11:00 +0530

    .

--- costumes.py
+++ costumes.py
@@ -2,8 +2,8 @@ import sys
 
 from PIL import Image 
 images=[]
-for arg in sys.argv[1:]:
-    image=Image.open(arg)
+for argr in sys.argv[1:]:
+    image=Image.open(argr)
     images.append(image)
 images[0].save(
     "costumes.gif",save_all=True,append_images=[images[1]],duration=200,loop=0
