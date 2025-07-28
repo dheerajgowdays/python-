5165f90d01a05053d467e6ad37947ba785dfb6bd
dheerajgowdays <dheeraj76766@gmail.com>
2025-07-28 19:39:21 +0530

    .

--- costumes.py
+++ costumes.py
@@ -1,10 +1,16 @@
 import sys
 
-from PIL import Image 
-images=[]
-for argr in sys.argv[1:]:
-    image=Image.open(argr)
+from PIL import Image
+
+if len(sys.argv) < 3:
+    print("Usage: python costumes.py image1 image2 [image3 ...]")
+    sys.exit(1)
+
+images = []
+for arg in sys.argv[1:]:
+    image = Image.open(arg)
     images.append(image)
+
 images[0].save(
-    "costumes.gif",save_all=True,append_images=[images[1]],duration=400,loop=0
+    "costumes.gif", save_all=True, append_images=images[1:], duration=400, loop=0
 )
