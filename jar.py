a95f2e9189ae153cfa264435ec75ef155372a76e
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2025-08-27 01:56:27 +0530

    Update jar.py

--- jar.py
+++ jar.py
@@ -1,6 +1,6 @@
 class Jar:
     def __init__(self, capacity=12):
-        if capacity <= 0:
+        if capacity < 0:
             raise ValueError("Capacity should be non-negative")
         self.capacity = capacity
         self.size=0
