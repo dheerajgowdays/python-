e1ea70b23325f652d8bf3890c206121502553da1
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2025-08-27 01:02:31 +0530

    .

--- seasons.py
+++ seasons.py
@@ -1,19 +1,32 @@
 from datetime import date , datetime
 import sys
+import operator
 import inflect
+p = inflect.engine()  
 class Time:
-    def __init__(self):
-        pass
+    minutes = 0
+    def __init__(self,bod,day):
+        self.bod = bod
+        self.day = day
+        self.minutes = Time.cal_time(bod,day) 
+    @classmethod
+    def cal_time(cls,bod,day):
+        delta = operator.__sub__(day,bod)
+        days = delta.days
+        min = days * 24 * 60
+        word = p.number_to_words(min, andword="").capitalize()
+        return word  
 
 def main():
-    bod1 = get_bod()
-    
-
-def get_bod():
     bod=input("Date Of Birth: ").replace("/","-")
+    bod1 = get_bod(bod)
+    print(bod1.minutes)
+
+def get_bod(bod):
     try:
         bod = datetime.strptime(bod, "%Y-%m-%d").date()
-        return Time(bod)
+        day = date.today()
+        return Time(bod,day)
     except ValueError:
         print("Invalid Input")
         sys.exit(1)
