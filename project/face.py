79cce6182510e6435b7e7a5be4854e5490a1eda5
Dheeraj Gowda YS <dheeraj76766@gmail.com>
2025-09-08 23:09:35 +0530

    .

--- project/face.py
+++ project/face.py
@@ -10,19 +10,18 @@ while True:
     c_rec, d_image = b.read()  # Read frame from webcam
     e = cv2.cvtColor(d_image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
 
-    # Detect faces
     f = a.detectMultiScale(e, scaleFactor=1.3, minNeighbors=6)
 
     # Draw rectangle around detected faces
     for (x1, y1, w1, h1) in f:
-        cv2.rectangle(d_image, (x1, y1), (x1 + w1, y1 + h1), (255, 0, 0), 5)
+        cv2.rectangle(d_image,(x1, y1),(x1 + w1, y1 + h1),(256, 0, 0), 5)
 
     # Show video frame
     cv2.imshow('img', d_image)
 
     # Press 'q' to quit
-    h = cv2.waitKey(40) & 0xff
-    if h == ord('q'):
+    d = cv2.waitKey(40) & 0xff
+    if d == ord('q'):
         break
 
 b.release()
